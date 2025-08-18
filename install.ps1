d37ee2e2e51e8a3fd77bb156a34ed19d40cf1123
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-08-18 12:38:38 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,7 +3,7 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
 scoop bucket add extras
-scoop install git python fzf fd bat neovim gcc clangd make cmake komorebi autohotkey
+scoop install git python fzf fd bat neovim gcc clangd make cmake komorebi autohotkey gdb
 
 git clone https://github.com/connor-mccartney/deploy-arch-hyprland
 mv .\deploy-arch-hyprland\dotfiles\.config\nvim\ C:\Users\crm36\AppData\Local\
