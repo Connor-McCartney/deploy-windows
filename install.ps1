9e8c4ad271c695b832ab2f001dddc8aa53caddc8
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-08-17 20:11:54 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,3 +3,6 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
 scoop install git python fzf neovim
+
+git clone https://github.com/connor-mccartney/deploy-arch-hyprland
+mv .\deploy-arch-hyprland\dotfiles\.config\nvim\ C:\Users\crm36\AppData\Local\
