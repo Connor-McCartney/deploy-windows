989f496397d539ce52816c99d6f4b4fcc745a1a4
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-08-18 00:52:59 -0700

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -2,8 +2,29 @@ if ($Host.Name -ne 'ConsoleHost') {return}
 cd ~/Documents
 
 
-
-
-
 Set-Alias l ls
-Set-Alias v neovide
+Set-Alias c cd
+#Set-Alias v neovide
+Set-Alias v nvim
+
+
+function fzf {
+    return & fzf.exe --bind "ctrl-o:down,tab:accept" --preview "bat --color=always {1}"
+}
+
+function cc {
+    cd ~
+    $env:FZF_DEFAULT_COMMAND = "fd --type d --strip-cwd-prefix --hidden --follow --exclude .git"
+    $selection = fzf
+    cd $selection
+}
+
+function vv {
+    cd ~
+    $env:FZF_DEFAULT_COMMAND = "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
+    $selection = fzf
+
+    $dir = Split-Path $selection
+    cd $dir
+    v "$HOME\$selection"
+}
