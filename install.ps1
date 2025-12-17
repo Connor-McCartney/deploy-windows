a2259c78e90f14287e4fb5a9e837b60dbf1fb99c
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-12-17 12:57:40 +1100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,8 +2,9 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
+scoop install git # required to install buckets
 scoop bucket add extras
-scoop install git python fzf fd bat neovim gcc clangd make cmake autohotkey gdb which glazewm
+scoop install python fzf fd bat neovim gcc clangd make cmake autohotkey gdb which glazewm
 
 git clone https://github.com/connor-mccartney/deploy-arch-hyprland
 mv .\deploy-arch-hyprland\dotfiles\.config\nvim\ C:\Users\crm36\AppData\Local\
